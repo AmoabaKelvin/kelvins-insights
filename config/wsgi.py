f5db00c485a82fc613734539a960b56ab8e998f9
Kelvin Amoaba <kel.amoaba@gmail.com>
2022-06-10 14:17:29 +0000

    Fixed default DJANGO_SETTINGS_MODULE pointing to a folder instead of file

--- config/wsgi.py
+++ config/wsgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
 import os
+from dotenv import load_dotenv
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+load_dotenv()
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.production')
 
 application = get_wsgi_application()
