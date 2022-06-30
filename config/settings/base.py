98a346001ac62574cfecbbe200ae2130e18aa81e
Kelvin Amoaba <kel.amoaba@gmail.com>
2022-06-30 14:58:28 +0000

    Added cloudinary storage before django staticfiles. This is to make cloudinary host the static files.

--- config/settings/base.py
+++ config/settings/base.py
@@ -23,9 +23,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.sites",
-    # cloudinary integration for media files only
-    "django.contrib.staticfiles",
     "cloudinary_storage",
+    "django.contrib.staticfiles",
     # custom apps
     "blog",
     "portfolio",
@@ -36,7 +35,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",
+    # "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
