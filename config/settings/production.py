26786c5cdc9e3fea06808d47ff0a79aa347d4252
Kelvin Amoaba <kel.amoaba@gmail.com>
2022-06-30 15:32:27 +0000

    Changed static_root location and staticfilesdirs location

--- config/settings/production.py
+++ config/settings/production.py
@@ -25,7 +25,7 @@ DATABASES["default"] = dj_database_url.config(conn_max_age=600, ssl_require=True
 # configuring static files for production environment
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "staticfiles"),
 ]
 STATICFILES_STORAGE = "cloudinary_storage.storage.StaticHashedCloudinaryStorage"
 
