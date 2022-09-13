4ebd086e063e16de7a65157beef22a91687e6508
Kelvin Amoaba <kel.amoaba@gmail.com>
2022-09-13 02:06:36 +0000

    Removed unused imports. Changed function based views to Class based views except for the like_a_blog_post_function.

--- blog/views.py
+++ blog/views.py
@@ -1,20 +1,21 @@
 from django.contrib import messages
 from django.http import HttpResponseBadRequest, JsonResponse
 from django.shortcuts import get_object_or_404, redirect, render
+from django.urls import reverse
 from django.views import View
 from django.views.decorators.csrf import csrf_exempt
-from django.views.decorators.http import require_http_methods
 from django.views.generic import ListView
 from portfolio.models import Portfolio
 
-from .forms import BlogCommentForm
 from .models import BlogLikes, BlogPost, NewsletterSubscribers
 from .utils import get_client_ip
 
 
 class ListBlogPosts(ListView):
-    model = BlogPost
-    template_name = "blog/index.html"
+    # get each of the blog posts from the database and prefetch their tags
+    # and order them by the date they were added in descending order
+    queryset = BlogPost.objects.prefetch_related("tags").order_by("-date_added")
+    template_name = "blog/index-new.html"
     paginate_by = 30
     ordering = "-date_added"
 
@@ -31,54 +32,40 @@ class BlogDetailView(View):
     """Detail view for blog comment, the blog is obtained using the slug."""
 
     def get(self, request, *args, **kwargs):
-        blog = get_object_or_404(BlogPost, slug=kwargs["slug"])
-        form = BlogCommentForm()
-        self.portfolio = Portfolio.objects.first()
+        blog = get_object_or_404(
+            BlogPost.objects.prefetch_related("tags"), slug=kwargs["slug"]
+        )
         self.ip_addresses = [i.ip_address for i in BlogLikes.objects.filter(post=blog)]
         return render(
             request,
-            "blog/blog-post.html",
+            "blog/detail.html",
             {
-                "form": form,
                 "blog": blog,
-                "portfolio": self.portfolio,
                 "address": self.ip_addresses,
             },
         )
 
+
+class NewsletterView(View):
+    """
+    View to handle the addition of an email to the list of newsletter
+    substribers
+    """
+
     def post(self, request, *args, **kwargs):
-        form = BlogCommentForm(request.POST)
-        blog = get_object_or_404(BlogPost, slug=kwargs["slug"])
-        if form.is_valid():
-            form.save(commit=False)
-            form.instance.blog = blog
-            form.save()
-            return redirect(request.path)
-        return HttpResponseBadRequest("Invalid form data")
-
-
-def add_user_to_newsletter(request):
-    """Add user to newsletter subscription"""
-    if request.method == "POST":
-        email = request.POST.get("semail")
+        homepage: str = reverse("index")
+        email: str = request.POST.get("user_email")
         if email:
-            # process the obtained email address by checking t
-            # if it already exists in the database before adding it.
-            # If it does, then no need to add it again, so we
-            # return an alert indicating that its already present.
-            # If it doesn't, then we add it to the database and return a
-            # success alert, redirecting the user to the previous screen.
             if NewsletterSubscribers.objects.filter(email=email).exists():
-                messages.error(request, "Email already exists")
-                print(request.path)
-                return redirect(request.META.get("HTTP_REFERER", "/"))
+                messages.error(request, "Email alread exists")
+                return redirect(homepage)
             NewsletterSubscribers.objects.create(email=email)
             messages.success(
                 request,
-                "Email added to newsletter. You will be notified when new posts are published.",
+                "Thank you for subscribing to my newsletter, you will be the first to know when I publish a new blog post.",
             )
-            return redirect(request.META.get("HTTP_REFERER", "/"))
-    return render(request, "blog/index.html")
+            return redirect(homepage)
+        return render(request, "blog/index-new.html")
 
 
 @csrf_exempt
@@ -102,22 +89,19 @@ def like_a_blog_post(request, post_id):
     return HttpResponseBadRequest("Invalid request")
 
 
-@require_http_methods(["GET"])
-def search_blog_posts(request):
-    """This view handles the search of blog posts."""
-    # The name of the HTML input widget is called `search`
-    # For now, the user's keyword is searched against the title of the blogs
-    # present in the database.
-    search_term = request.GET["search"]
-    if search_term:
-        # We need to search the database for the search term
-        # and return the results.
-        results = BlogPost.objects.filter(title__icontains=search_term)
-        portfolio = Portfolio.objects.first()
+class BlogSearchView(View):
+    """
+    Handle the searching of a blog post
+    """
+
+    def get(self, request, *args, **kwargs):
+        search_term = request.GET["search"]
+        results_queryset = BlogPost.objects.filter(title__icontains=search_term)
         return render(
             request,
-            "blog/search-results.html",
-            {"blogs": results, "search_term": search_term, "portfolio": portfolio},
+            "blog/index-new.html",
+            {
+                "blogs": results_queryset,
+                "search_term": search_term,
+            },
         )
-    messages.error(request, "Please enter a search term.")
-    return redirect("index")
