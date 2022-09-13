7cc201a3685dec73704d20b3af729accb5f6d18e
Kelvin Amoaba <kel.amoaba@gmail.com>
2022-09-13 02:14:47 +0000

    Custom JS for website

---
+++ staticfiles/assets/js/custom.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+    // Add smooth scrolling to all links
+    $("a").on('click', function(event) {
+
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function(){
+
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+        } // End if
+    });
+
+    // Show the navbar when the user clicks the menu icon
+    $(".nav__toggle").click(function() {
+        $(".nav__toggle__icons").fadeToggle();
+        $(".nav__toggle__icons").css("display", "flex");
+    });
+
+    // Add an event to close the thanks for subscribing message
+    $("#close_alert").click(function() {
+        $(".alert_container").hide();
+    }
+    );
+});
