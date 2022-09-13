61c2854458740d843adf893ea40bdcea1bfcbbec
Kelvin Amoaba <kel.amoaba@gmail.com>
2022-09-13 02:11:43 +0000

    Direct to class based view instead of function based view

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.portfolio, name="portfolio"),
+    path("", views.AboutView.as_view(), name="portfolio"),
     path("contact/", views.get_in_touch_view, name="get_in_touch"),
 ]
