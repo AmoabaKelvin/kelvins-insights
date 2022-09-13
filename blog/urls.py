08cba2b79d8568194262e2cccc7d30ce9eeb812c
Kelvin Amoaba <kel.amoaba@gmail.com>
2022-09-13 02:04:01 +0000

    Replaced function based views with class based views

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.ListBlogPosts.as_view(), name="index"),
     path("blog/<slug:slug>", views.BlogDetailView.as_view(), name="detail"),
-    path("newsletter/", views.add_user_to_newsletter, name="newsletter"),
+    path("newsletter/", views.NewsletterView.as_view(), name="newsletter"),
     path("like/<int:post_id>", views.like_a_blog_post, name="like_a_blog_post"),
-    path("search/", views.search_blog_posts, name="search"),
+    path("search/", views.BlogSearchView.as_view(), name="search"),
 ]
