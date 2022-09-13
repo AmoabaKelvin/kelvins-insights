4d629209a3318198330e331062d2d49ebcc03d0b
Kelvin Amoaba <kel.amoaba@gmail.com>
2022-09-13 02:09:20 +0000

    Changed database configurations and also email backend

--- config/settings/local.py
+++ config/settings/local.py
@@ -4,9 +4,13 @@ ALLOWED_HOSTS = ["*"]
 
 DATABASES = {
     "default": {
-        # for local development, use sqlite3
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        # for local development, use postgresql
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "kelvins_insights",
+        "USER": "postgres",
+        "PASSWORD": os.environ["DB_PASSWORD"],
+        "HOST": "localhost",
+        "PORT": "5433",
     }
 }
 
@@ -18,13 +22,13 @@ MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
-EMAIL_HOST = "localhost"
-EMAIL_USE_TLS = False
-DEFAULT_FROM_EMAIL = "admin@localhost"
-EMAIL_HOST_USER = "admin@localhost"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = "kelvinsinsights@gmail.com"
+EMAIL_HOST_USER = "kelvinsinsights@gmail.com"
 EMAIL_HOST_PASSWORD = os.environ["EMAIL_PASSWORD"]
-EMAIL_PORT = 25
+EMAIL_PORT = 587
 
 
 CELERY_BROKER_URL = "redis://localhost:6379"
